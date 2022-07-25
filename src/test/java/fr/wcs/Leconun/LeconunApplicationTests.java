c0069046b6d81e812a453440073b6dd5859f5f8e
clem-mal <clement.mallambic@laposte.net>
2022-07-25 16:43:46 +0200

    lecon1

---
+++ src/test/java/fr/wcs/Leconun/LeconunApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.wcs.Leconun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeconunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
